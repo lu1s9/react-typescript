bcd2e09e43226cee3e288089c1f6d1c99badea15
Luis Fernando <sloopy.max@gmail.com>
2024-05-11 17:52:58 -0600

    feat: add axios requests for posts

---
+++ src/api/posts.ts
@@ -0,0 +1,8 @@
+import axios from "./axios";
+
+export const getPosts = () => axios.get("/posts");
+
+export const createPost = (post: { content: string }) =>
+  axios.post("/posts", post);
+
+export const deletePost = (id: string) => axios.delete(`/posts/${id}`);
