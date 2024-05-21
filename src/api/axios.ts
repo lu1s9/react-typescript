8d6283409af7ca5f37407f1be3d7bd47394fb8cc
Luis Fernando <sloopy.max@gmail.com>
2024-05-20 20:15:42 -0600

    refactor: use api url versioning

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,7 +4,7 @@ import Cookies from "js-cookie";
 const token = Cookies.get("token");
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "http://localhost:3001/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
