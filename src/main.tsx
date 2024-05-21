94bb35426846671c56ff3538cf66795939e9c548
Luis Fernando <sloopy.max@gmail.com>
2024-05-20 20:21:17 -0600

    feat: add user context to app

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import App from "./App.tsx";
 import "./index.css";
 import { PostsContextProvider } from "./context/PostContext.tsx";
 import { AuthContextProvider } from "./context/AuthContext.tsx";
+import { UsersContextProvider } from "./context/UserContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthContextProvider>
       <PostsContextProvider>
-        <App />
+        <UsersContextProvider>
+          <App />
+        </UsersContextProvider>
       </PostsContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
