b8426bd34eb2c7c6a9d604a781d326f6ff5b44de
Luis Fernando <sloopy.max@gmail.com>
2024-05-20 20:17:04 -0600

    feat: add friendlist link to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,13 +12,17 @@ function Navbar() {
 
   return (
     <header className="text-gray-700 bg-white">
-      <div className="max-w-7xl mx-auto px-10 py-14 flex items-center justify-between">
+      <div className="max-w-7xl mx-auto px-10 py-4 flex items-center justify-between">
         <Link to="/" className="text-gray-700  no-underline">
-          <h1>Social Network</h1>
+          <h1 className="text-3xl font-extrabold">Social Network</h1>
         </Link>
         <nav className="flex items-center">
-          {state.token && (
+          {state.user && (
             <div>
+              <span>{state.user.name}</span>
+              <Link to="friendsList" className="ml-3">
+                Friends List
+              </Link>
               <button
                 onClick={handleClick}
                 className="text-green-500 bg-white border-solid border-green-500 border-2 rounded cursor-pointer text-base py-2 px-3"
@@ -28,7 +32,7 @@ function Navbar() {
             </div>
           )}
 
-          {!state.token && (
+          {!state.user && (
             <div>
               <Link to="/login" className="ml-3">
                 Log in
